1639b6b14712538a4c34817562642cb9e651cfa8
Your Name <you@example.com>
2025-04-10 21:33:21 +0300

    Update

--- scripts/mouse.js
+++ scripts/mouse.js
@@ -1,20 +1,42 @@
 //Мишка
 var keys = document.querySelectorAll(".key");
+let currentNoteSound;
 
 keys.forEach((key) => {
   let noteSound;
 
   key.addEventListener("mousedown", () => {
     var note = key.getAttribute("data-note");
+
+    if (currentNoteSound) {
+      currentNoteSound.pause();
+      currentNoteSound.currentTime = 0;
+    }
+
     noteSound = new Audio(`./notes/${note}.mp3`);
     noteSound.play();
 
+    currentNoteSound = noteSound; 
+
+    key.classList.add('active');
     console.log("mouse touch");
   });
 
   key.addEventListener("mouseup", () => {
     if (noteSound) {
-      noteSound.pause();
+      // noteSound.pause();
+      key.classList.remove('active');
+
+      console.log("mouse untouch");
+    }
+  });
+
+  key.addEventListener("mouseleave", () => {
+    if (noteSound) {
+      // noteSound.pause();
+      key.classList.remove('active');
+
+      console.log("mouse leave");
     }
   });
 });
