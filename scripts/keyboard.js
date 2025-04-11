a743d8033c42f212434e621c536713be9f283a2c
Your Name <you@example.com>
2025-04-11 23:21:53 +0300

    Update

--- scripts/keyboard.js
+++ scripts/keyboard.js
@@ -2,15 +2,14 @@
 let activeKeys = {};
 
 document.addEventListener("keydown", (event) => {
-  const pressedKey = event.code;
 
-  if (activeKeys[pressedKey]) return false; //блокування тих самих нот
+  if (activeKeys[event.code]) return false; //блокування тих самих нот
 
   keys.forEach((key) => {
     const dataCode = key.getAttribute("data-code");
     const altdataCode = key.getAttribute("alternative-data-code");
 
-    if (pressedKey === dataCode || pressedKey === altdataCode) {
+    if (event.code === dataCode || event.code === altdataCode) {
       const note = key.getAttribute("data-note");
 
       var instrumentSelector = document.getElementById("instrument");
@@ -28,7 +27,7 @@ document.addEventListener("keydown", (event) => {
       noteSound.volume = volumeSlider.value / 100;
       noteSound.play();
 
-      activeKeys[pressedKey] = [noteSound];
+      activeKeys[event.code] = noteSound;
 
       key.classList.add("active");
       console.log("keyboard touch");
@@ -36,22 +35,20 @@ document.addEventListener("keydown", (event) => {
   });
 });
 
-document.addEventListener("keyup", (event) => {
-  let keyCode = event.code;
+document.addEventListener("keyup", function(event) {
 
-  if (activeKeys[keyCode]) {
-    let sounds = activeKeys[keyCode];
+  if (activeKeys[event.code]) {
+    let sound = activeKeys[event.code];
+    fadeOutAudio(sound);
 
-    for (let i = 0; i < sounds.length; i++) {
-      let sound = sounds[i];
-      fadeOutAudio(sound);
+    delete activeKeys[event.code];
+
+    let keyElement = document.querySelector('[data-code="' + event.code + '"]') 
+    || document.querySelector('[alternative-data-code="' + event.code + '"]');
+
+    if (keyElement !== null) {
+      keyElement.classList.remove("active");
     }
-
-    delete activeKeys[keyCode];
-
-    let keyElement = document.querySelector('[data-code="' + keyCode + '"], [alternative-data-code="' + keyCode + '"]');
-    
-    keyElement.classList.remove("active");
     console.log("keyboard up");
   }
 });
