3a9c7aecca4165e8b5a9a1794d642e91619cb014
Your Name <you@example.com>
2025-04-12 23:37:15 +0300

    Update

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,24 +1,25 @@
-//щоб не було затримки
-const preloadNotes = () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const notes = [
+    "A#4", "A#5", "A4", "A5", "B4", "B5", "C#4", "C#5", "C#6", "C4", "C5", "C6",
+    "D#4", "D#5", "D#6", "D4", "D5", "D6", "E4", "E5", "E6", "F#4", "F#5", "F#6",
+    "F4", "G#4", "G#5", "G4", "G5", "G6"
+  ];
+
+  const preloadNotes = () => {
     const instruments = ["piano", "celesta"];
-    const notes = [
-      "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
-      "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
-      "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6"
-    ];
-  
     instruments.forEach((instrument) => {
       notes.forEach((note) => {
-        let notePath;
-        if (instrument === "piano") {
-          notePath = `./notes/${note}.mp3`;
-        } else {
-          notePath = `./notes celesta/${note}.wav`;
-        }
+        const encodedNote = note.replace("#", "%23");
+        const notePath = instrument === "piano" 
+          ? `./notes/${encodedNote}.mp3` 
+          : `./notes celesta/${encodedNote}.wav`;
         const audio = new Audio(notePath);
-        audio.load();
+        audio.volume = 0.0001;
+        audio.play().catch(() => {
+        });
       });
     });
   };
-  
-  document.addEventListener("DOMContentLoaded", preloadNotes);
+
+  preloadNotes();
+});
